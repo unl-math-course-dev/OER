392e346484f9f3d90e3516ea6d926ed96b22745e
Nathan Wakefield <43244840+nwakefield2@users.noreply.github.com>
2019-05-05 22:14:22 -0500

    Working on Admin Page

---
+++ Tools/index.php
@@ -0,0 +1,44 @@
+<?php 
+error_reporting(E_ERROR | E_PARSE); 
+
+echo "<html>
+  <head>
+    <title>UNL OER Administration Panel</title>
+  </head>
+  <body>";
+
+require_once "../phpCAS/CAS.php";
+ //Initialize phpCAS
+phpCAS::client(CAS_VERSION_2_0,'shib.unl.edu',443,'/idp/profile/cas');
+
+
+// For quick testing you can disable SSL validation of the CAS server.
+phpCAS::setNoCasServerValidation();
+
+if (isset($_REQUEST['logout'])) {
+    phpCAS::logout();
+}
+if (isset($_REQUEST['login'])) {
+    phpCAS::forceAuthentication();
+}
+// check CAS authentication
+$auth = phpCAS::checkAuthentication();
+
+if ($auth) {
+
+echo "<h1>Welcom</h1>";
+
+
+}  else {
+                                        
+    echo "<h1>Please Login</h1>";
+    echo "<p><a href=\"?login=\">Login</a></p>";
+}
+
+    echo "
+    
+    <a href=\"index.php\">Home</a>
+    
+    
+    <p><a href=\"?logout=\">Logout</a></p> </body>";
+?>
