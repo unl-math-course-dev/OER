a694ab5a35147e8fbb71a390eb9ecb037660e63c
Nathan Wakefield <43244840+nwakefield2@users.noreply.github.com>
2019-05-05 22:46:25 -0500

    Update index.php

--- Tools/index.php
+++ Tools/index.php
@@ -29,12 +29,12 @@ $uname=phpCAS::getUser();
 echo "<h1>Welcom</h1>";
 $admins = array('nwakefield2');
 
-if (in_array($uname , $admins)){
-echo "You are logged in as ";
-} else {
-echo "You are logged in as  but you are not allowed toa ccess this resource.  If you believe you need access please contact Nathan";
+//if (in_array($uname , $admins)){
+//echo "You are logged in as ";
+//} else {
+//echo "You are logged in as  but you are not allowed toa ccess this resource.  If you believe you need access please contact Nathan";
 
-}
+// }
 
 
 }  else {
