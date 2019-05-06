f01e18e8c83fc87a275a38678f2a5b1fba9c8a98
Nathan Wakefield <43244840+nwakefield2@users.noreply.github.com>
2019-05-05 22:23:09 -0500

    Update index.php

--- Tools/index.php
+++ Tools/index.php
@@ -7,6 +7,22 @@ echo "<html>
   </head>
   <body>";
 
+require_once "../phpCAS/CAS.php";
+ //Initialize phpCAS
+phpCAS::client(CAS_VERSION_2_0,'shib.unl.edu',443,'/idp/profile/cas');
+
+
+// For quick testing you can disable SSL validation of the CAS server.
+phpCAS::setNoCasServerValidation();
+
+if (isset($_REQUEST['logout'])) {
+    phpCAS::logout();
+}
+if (isset($_REQUEST['login'])) {
+    phpCAS::forceAuthentication();
+}
+// check CAS authentication
+$auth = phpCAS::checkAuthentication();
 
 
     echo "
