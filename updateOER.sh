e7d2a69f633216e17f99973e7273d15af96e3fb7
Nathan Wakefield <43244840+nwakefield2@users.noreply.github.com>
2019-05-10 15:03:07 -0500

    Update updateOER.sh

--- updateOER.sh
+++ updateOER.sh
@@ -4,6 +4,9 @@ cd /var/www/
 
 git pull
 
+
+cd /var/www/html/PreCalculus/
+
 xsltproc --xinclude /var/www/html/PreCalculus/mfg-html.xsl /var/www/OERSource/PreCalc/2019-2020/update.xml 
 
 perl /var/www/html/PreCalculus/GoogleAnalytics.pl 
