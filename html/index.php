b57af6bd34bcd9d3902f644bbe10a1b93e2b7441
Nathan Wakefield <nwakefield2@unl.edu>
2019-05-13 12:48:26 -0500

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,10 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Open Source Mathematics Textbooks at The University of Nebraska - Lincoln</h1>
 <?php
 $dir = ".";
 
 // Sort in ascending order - this is default
 $a = scandir($dir);
 
+foreach($a as $title){
+echo "<h2>".$title."</h2>"
+
+}
 
 var_dump($a);
 
 ?>
+
+</body>
+</html>
