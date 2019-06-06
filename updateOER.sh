74827f77199547b17065a1bd3b10b0e5f7e74bb4
nwakefield2 <43244840+nwakefield2@users.noreply.github.com>
2019-06-06 18:23:01 -0500

    Update updateOER.sh

--- updateOER.sh
+++ updateOER.sh
@@ -8,13 +8,13 @@ cd /var/www/
 git pull
 
 #Move into the Precalculus html directory
-cd /var/www/html/PreCalculus/
+#cd /var/www/html/PreCalculus/
 
 #Run xsltproc
-xsltproc --xinclude /var/www/html/PreCalculus/mfg-html.xsl /var/www/OERSource/PreCalc/2019-2020/update.xml 
+#xsltproc --xinclude /var/www/html/PreCalculus/mfg-html.xsl /var/www/OERSource/PreCalc/2019-2020/update.xml 
 
 #Add the Google Analytics code
-perl /var/www/html/PreCalculus/GoogleAnalytics.pl 
+#perl /var/www/html/PreCalculus/GoogleAnalytics.pl 
 
 
 #Move into the Calculus html directory
@@ -27,13 +27,13 @@ xsltproc --xinclude /var/www/html/Calculus/mfg-html.xsl /var/www/OERSource/Calc/
 perl /var/www/html/Calculus/GoogleAnalytics.pl 
 
 #Move into the Contemporary html directory
-cd /var/www/html/Contemporary/
+#cd /var/www/html/Contemporary/
 
 #Run xsltproc
-xsltproc --xinclude /var/www/html/Contemporary/mfg-html.xsl /var/www/OERSource/Contemporary/2019-2020/index.xml 
+#xsltproc --xinclude /var/www/html/Contemporary/mfg-html.xsl /var/www/OERSource/Contemporary/2019-2020/index.xml 
 
 #Add the Google Analytics code
-perl /var/www/html/Contemporary/GoogleAnalytics.pl 
+#perl /var/www/html/Contemporary/GoogleAnalytics.pl 
 
 
 echo "OER Compile Successful: $(date)" >> /var/www/OERupdate.log
