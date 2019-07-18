d6cc54230601a94b38996c26608a18ee230e4e1f
Nathan Wakefield <nwakefield2@unl.edu>
2019-07-18 10:49:13 -0500

    Create index.php

---
+++ html/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {background-color: #fefdfa;
+color:#fefdfa;
+   margin:0;
+   padding:0;
+}
+
+img {
+    opacity: 1.0;
+    filter: alpha(opacity=30); /* For IE 8 & 9 (more valid) */
+}
+
+#wrapper, img{
+    width:100%;
+    height: 100%;
+}
+
+table{width:100%;
+margin: 0px;}
+
+th {
+  border-right: 1px solid #ddd;
+  text-align: center;
+  vertical-align: center;
+  background-color: #d00000
+}
+table th:last-child {
+    width: 75%;
+}
+
+
+h1 {
+  color: #242423;
+}
+
+h3 {
+  color: #fefdfa;
+}
+
+a {
+  color: #fefdfa;
+}
+
+
+.dropdown {
+  padding: 15px;
+  position: relative;
+
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #d00000;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px #424240;
+  padding: 12px 16px;
+  z-index: 1;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+
+
+</style>
+</head>
+<body>
+<table>
+ <th>
+ 
+ </th>
+</table>
+
+
+
+<center>
+<h1>Open Source Mathematics Textbooks <br> at The University of Nebraska - Lincoln</h1>
+</center>
+<?php
+
+?>
+
+
+
+<table>
+
+
+  <tr>
+  
+  
+  <th class="dropdown">
+  <span><h3>Open Source Textbooks</h3></span>
+  <div class="dropdown-content">
+  <p><a href="http://mathbooks.unl.edu/PreCalculus">PreCalculus</a></p>
+  <p><a href="http://mathbooks.unl.edu/Calculus">Calculus</a></p>
+  <p><a href="http://mathbooks.unl.edu/Contemporary">Contemporary Mathematics</a></p>
+  </div>
+  </th>
+  
+    <th class="dropdown">
+  <span><h3>Departmental Tools</h3></span>
+  <div class="dropdown-content">
+  <p><a href="http://mathbooks.unl.edu/FYM">First Year Mathematics Program Wiki</a></p>
+  <p><a href="https://math-webwork2.unl.edu/webwork2/">WeBWorK</a></p>
+  </div>
+  </th>
+  
+      <th class="dropdown">
+  <span><h3>External Tools</h3></span>
+  <div class="dropdown-content">
+  <p><a href="https://www.geogebra.org">Geogebra</a></p>
+  <p><a href="https://www.desmos.com">Desmos</a></p>
+  </div>
+  </th>
+
+<th>
+
+</th>
+  
+  </tr>
+
+
+<tr>
+<td colspan="100%">
+<div id="wrapper">
+<img src="campus.jpg" alt="Picture of the UNL Campus">
+</div>
+<td>
+</tr>
+</table>
+
+
+
+
+
+
+
+</body>
+</html>
