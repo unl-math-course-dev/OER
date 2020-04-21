ffcf836fa4d27a8f9223686ad054eccd14def022
Nathan Wakefield <43244840+nwakefield2@users.noreply.github.com>
2020-04-21 07:29:02 -0500

    Create updatePC.sh

---
+++ updatePC.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#Make sure we are in the right directory on the server
+cd /var/www/
+
+#Get the latest update from github
+git pull
+
+#Move into the Precalculus html directory
+cd /var/www/html/PreCalculus/
+
+#Run xsltproc
+xsltproc --xinclude /var/www/html/PreCalculus/mfg-html.xsl /var/www/OERSource/PreCalc/2019-2020/update.xml 
+
+#Add the Google Analytics code
+perl /var/www/html/PreCalculus/GoogleAnalytics.pl 
+
+
+
+
+echo "OER Compile Successful: $(date)" >> /var/www/OERupdate.log
