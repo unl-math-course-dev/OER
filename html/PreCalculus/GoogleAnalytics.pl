1a11ec8f116742c6d2d8d4a7024e7a6c368d65e8
Nathan Wakefield <43244840+nwakefield2@users.noreply.github.com>
2020-07-06 17:57:14 -0500

    Create GoogleAnalytics.pl

---
+++ html/PreCalculus/GoogleAnalytics.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl		
+
+  use strict;		
+ use warnings;		
+ use File::Slurp qw(read_file write_file);		
+
+  my @files = <*.html>;		
+ foreach my $file (@files){		
+
+  my $filename = $file;		
+ $filename =~ s/\.html/.html/g;		
+
+  #print $filename;		
+ #print "\n";		
+
+  my $data = read_file $file, {binmode => ':utf8'};		
+
+ 
+   $data =~ s/[^[:ascii:]]//g;		
+
+   if ($data =~ m/UA-122864107-1/)		
+  {		
+  print " ";		
+   #print "found the tracking code already present\n"; 		
+   }		
+   else		
+ { 		
+   $data =~ s/<head>/<head>\n<script async src=\"https:\/\/www.googletagmanager.com\/gtag\/js?id=UA-141543430-1\"><\/script> <script> window.dataLayer = window.dataLayer \|\| \[\]; function gtag(){dataLayer.push(arguments);} gtag(\'js\', new Date()); gtag(\'config\', \'UA-141543430-1\');<\/script>/gs;		
+ #print "added the tracking code\n";		
+
+   }		
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  write_file $filename, {binmode => ':utf8'}, $data;		
+ }		
+ #print "added the tracking code\n";		
+ #Invoke with perl FormatV2.pl *.txt		
+
+  print "\n"; 
