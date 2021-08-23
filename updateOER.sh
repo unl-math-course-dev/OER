58e0cc87979599113c5fec0971f4d1c861c1ec0e
nwakefield2 <43244840+nwakefield2@users.noreply.github.com>
2021-08-23 13:29:23 -0500

    Update updateOER.sh

--- updateOER.sh
+++ updateOER.sh
@@ -7,15 +7,6 @@ cd /var/www/
 #Get the latest update from github
 git pull
 
-#Move into the Precalculus html directory
-#cd /var/www/html/PreCalculus/
-
-#Run xsltproc
-#xsltproc --xinclude /var/www/html/PreCalculus/mfg-html.xsl /var/www/OERSource/PreCalc/2019-2020/update.xml 
-
-#Add the Google Analytics code
-#perl /var/www/html/PreCalculus/GoogleAnalytics.pl 
-
 
 #Move into the Calculus html directory
 cd /var/www/html/Calculus/
@@ -26,14 +17,6 @@ xsltproc --xinclude /var/www/html/Calculus/mfg-html.xsl /var/www/OERSource/Calc/
 #Add the Google Analytics code
 perl /var/www/html/Calculus/GoogleAnalytics.pl 
 
-#Move into the Contemporary html directory
-cd /var/www/html/Contemporary/
-
-#Run xsltproc
-xsltproc --xinclude /var/www/html/Contemporary/mfg-html.xsl /var/www/OERSource/Contemporary/2020-2021/index.xml 
-
-#Add the Google Analytics code
-perl /var/www/html/Contemporary/GoogleAnalytics.pl 
 
 
 echo "OER Compile Successful: $(date)" >> /var/www/OERupdate.log
